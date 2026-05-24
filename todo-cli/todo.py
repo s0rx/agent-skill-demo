28b102a230f1916b539b1f8cadf91eba0a979638
sorx <rosztoczy@gmail.com>
2026-05-24 21:46:13 +0200

    Revert "adding stuff"
    This reverts commit 5a91e631229b510bd2fde5c1d6d4a5d667b6d401.

--- todo-cli/todo.py
+++ todo-cli/todo.py
@@ -47,33 +47,15 @@ def list_todos():
         print(f"{mark} {t['id']}. {t['text']}")
 
 
-def done_todo(todo_id):
-    """Mark a todo as completed by ID."""
-    todos = load_todos()
-    for t in todos:
-        if t["id"] == todo_id:
-            t["done"] = True
-            save_todos(todos)
-            print(f"Marked done: {t['text']}")
-            return
-    print(f"Todo with id {todo_id} not found")
-
-
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python todo.py [add <text> | list | done <id>]")
+        print("Usage: python todo.py [add <text> | list]")
         return
     cmd = sys.argv[1]
     if cmd == "add" and len(sys.argv) > 2:
         add_todo(" ".join(sys.argv[2:]))
     elif cmd == "list":
         list_todos()
-    elif cmd == "done" and len(sys.argv) > 2:
-        try:
-            todo_id = int(sys.argv[2])
-            done_todo(todo_id)
-        except ValueError:
-            print(f"Error: '{sys.argv[2]}' is not a valid todo ID")
     else:
         print(f"Unknown command: {cmd}")
 
